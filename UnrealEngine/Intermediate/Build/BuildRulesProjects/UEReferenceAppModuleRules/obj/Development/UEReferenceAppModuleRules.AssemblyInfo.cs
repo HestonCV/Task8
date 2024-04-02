d14903e18eb2dffa2ea49ed02e6fbf1026604649
Brady Swann <cswann0332@gmail.com>
2024-04-02 10:13:05 -0500

    Cpp My hud class to BP
    Changed Cpp My HUD class to Blueprint

--- UnrealEngine/Intermediate/Build/BuildRulesProjects/UEReferenceAppModuleRules/obj/Development/UEReferenceAppModuleRules.AssemblyInfo.cs
+++ UnrealEngine/Intermediate/Build/BuildRulesProjects/UEReferenceAppModuleRules/obj/Development/UEReferenceAppModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UEReferenceAppModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf18e75974c20704bd30bf53baba3f1368fd6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbad7fd788ff5f0c146fe6cdfcc876f76ff52d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UEReferenceAppModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UEReferenceAppModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
